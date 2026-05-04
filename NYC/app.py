97735dc303c806dd58e5aa3fdf1f8a83830ab43c
Gowri <gowrikrishnakskv@gmail.com>
2026-05-04 21:24:58 +0530

    changes in the file

--- NYC/app.py
+++ NYC/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import joblib
 import numpy as np
 
+import os
+
 app = Flask(__name__)
 
 model = joblib.load("xgb_model.joblib")
@@ -60,4 +62,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
