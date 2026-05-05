7c3186f939d77584ea2bc41836535196bc772757
Gowri <gowrikrishnakskv@gmail.com>
2026-05-05 22:30:25 +0530

    changes in the file

--- NYC/app.py
+++ NYC/app.py
@@ -61,6 +61,7 @@ def predict():
         return jsonify({"error": str(e)})
 
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
